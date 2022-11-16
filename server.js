8d8716e34e3d5fe5bf72e1138db0c9d8762ec83c
Ayelet Hillel <ah3708@tc.columbia.edu>
2022-11-16 13:56:16 -0500

    budget data displayed on index

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require(`express`)
 const app = express()
 require(`dotenv`).config()
 const PORT = process.env.PORT || 3000
+app.use("/static", express.static("Public"))
+const Budget = require("./Models/budget")
+
+app.get("/budgets", (req, res) => {
+    res.render("index.ejs", { Budget })
+})
+
+app.get("/", (req, res) => {
+    res.send("This is working")
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Can you hear the ove on port ${PORT}?`)
